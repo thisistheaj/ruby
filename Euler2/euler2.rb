4a2926d0b7f1d22395fdf3bc59adf6063927dd5d
AJ Beckner <ajbeckner@gmail.com>
2016-08-23 23:40:57 -0400

    Added Euler 2

--- Euler2/euler2.rb
+++ Euler2/euler2.rb
@@ -1,33 +1,20 @@
 def fib(n)
   if n < 2
-     1
+    1
   elsif n >= 2
     return fib(n - 1) + fib(n - 2)
   end
 end
 
-def help
-  puts 'help - display this list of commands'
-  puts 'exit - ends the program'
-  puts 'fib <n> - prints "nth" fibonacci number'
-end
-
-if __FILE__ == $0
-  puts 'Welcome to fibonacci!'
-  puts 'Available Commands:'
-  help
-  $response = ''
-  while $response != "exit" do
-    print '>'
-    $response = gets.strip
-    if $response == 'help'
-      help
-    elsif $response.split(' ')[0] == 'fib' #if response has multiple args and forst is 'fib'
-      puts fib($response.split(' ')[1].to_i) #get the second arg, cast to int and pass to fib()
-    elsif $response == 'exit'
-
-    else
-      puts 'Bad Command, try again'
-    end
+i = 0
+sum = 0
+f = fib(i)
+while f < 4000000
+  if f % 2 == 0
+    sum += f
   end
+  i += 1
+  f = fib(i)
 end
+
+puts sum
